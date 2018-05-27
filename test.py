7d42aa26c48e6f7860beb4c4006addcdee0fc159
xim0608 <xim.ryuki@gmail.com>
2018-05-27 22:52:13 +0900

    update crawler

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import Firefox, FirefoxOptions
 from selenium.webdriver import PhantomJS
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
 import time
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15'
-delay = 10
+delay = 3
 
 
 def get_page_by_req(url):
@@ -20,7 +21,11 @@ def get_page_by_req(url):
     return r_text
 
 
-def get_page_by_sel(browser, wait, url, first_page=False):
+def get_page_by_sel(browser, url, first_page=False):
+    # options = FirefoxOptions()
+    # options.add_argument("-headless")
+    # browser = webdriver.Firefox()
+    wait = WebDriverWait(browser, delay)
     num = 0
     browser.get(url)
     browser.implicitly_wait(3)
@@ -28,26 +33,32 @@ def get_page_by_sel(browser, wait, url, first_page=False):
         el_present = EC.presence_of_element_located((By.ID, 'taplc_location_reviews_list_responsive_detail_0'))
         wait.until(el_present)
         if first_page:
-            num = browser.find_element_by_xpath(
+            number = browser.find_element_by_xpath(
                 '//*[@id="taplc_location_reviews_list_responsive_detail_0"]/div/p/b[1]')
+            num = int(number.text.replace(',', ''))
         print("Page is ready")
     except TimeoutException:
         print("Loading took too much time")
-
+    shade_invisible = EC.invisibility_of_element_located(
+        (By.CLASS_NAME, 'loadingShade'))
+    wait.until(shade_invisible)
+    # try:
+    #     el_present = EC.element_to_be_clickable((By.ID, 'taplc_location_review_keyword_search_modal_0_q'))
+    #     wait.until(el_present)
+    # except TimeoutException:
+    #     print("review load failed")
     try:
-        el_present = EC.element_to_be_clickable((By.ID, 'taplc_location_review_keyword_search_modal_0_q'))
-        wait.until(el_present)
-    except TimeoutException:
-        print("review load failed")
-
-    more_content_buttons = browser.find_elements_by_css_selector('span.taLnk.ulBlueLinks')
-    # more_content_buttons = browser.find_elements_by_xpath('//span[contains(text(), "さらに表示")]')
-    print(more_content_buttons)
-    if more_content_buttons:
-        for button in more_content_buttons:
-            print(button.text)
-            button.click()
-    invisible_more_content = EC.invisibility_of_element_located((By.XPATH, '//span[contains(text(), "さらに表示")]'))
+        more_content_buttons = browser.find_elements_by_css_selector('span.taLnk.ulBlueLinks')
+        # more_content_buttons = browser.find_elements_by_xpath('//span[contains(text(), "さらに表示")]')
+        print(more_content_buttons)
+        if more_content_buttons:
+            for button in more_content_buttons:
+                print(button.text)
+                button.click()
+    except StaleElementReferenceException:
+        print("error")
+    invisible_more_content = EC.invisibility_of_element_located(
+        (By.XPATH, '//span[contains(text(), "さらに表示") and contains(@class, "taLnk")]'))
     wait.until(invisible_more_content)
     elements = browser.find_elements_by_css_selector('.review.hsx_review')
 
@@ -56,8 +67,9 @@ def get_page_by_sel(browser, wait, url, first_page=False):
         print("content: {}".format(element.find_element_by_class_name('partial_entry').text))
         rating = element.find_element_by_class_name('ui_bubble_rating').get_attribute('class').split('_')[-1]
         print("star: {}".format(rating))
+    # browser.close()
+    return num
 
-    return int(num.text.replace(',', ''))
 
 def make_list(url, num):
     url_list = []
@@ -66,13 +78,11 @@ def make_list(url, num):
     return url_list
 
 
-
 if __name__ == '__main__':
     options = ChromeOptions()
     options.add_argument('--headless')
     # options.add_argument('--remote-debugging-port=9222')
     browser = Chrome(options=options)
-    wait = WebDriverWait(browser, delay)
     # num = 10
     # browser.implicitly_wait(1)
     url = 'https://www.tripadvisor.jp/Attraction_Review-g298173-d320009-Reviews-Minato_Mirai_21-Yokohama_Kanagawa_Prefecture_Kanto.html#REVIEWS'
@@ -82,11 +92,13 @@ if __name__ == '__main__':
     # # soups = soup.find_all("div", class_="entry")
     # # for t in soups:
     # #     print(t.get_text())
-    num = get_page_by_sel(browser, wait, url, first_page=True)
-    crawling_url_list = make_list(url, num)
-    for url in crawling_url_list:
-        time.sleep(1)
-        get_page_by_sel(browser, wait, url)
-    browser.close()
+    try:
+        num = get_page_by_sel(browser, url, first_page=True)
+        crawling_url_list = make_list(url, num)
+        for url in crawling_url_list:
+            time.sleep(1)
+            get_page_by_sel(browser, url)
+    finally:
+        browser.close()
     # print(num)
     # num = 1326
