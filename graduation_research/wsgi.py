24459e62b97b0d3fe3f8b0447686b076564ce5c0
xim0608 <xim.ryuki@gmail.com>
2018-06-01 10:32:05 +0900

    fix directory structure

--- graduation_research/wsgi.py
+++ graduation_research/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for research project.
+WSGI config for graduation_research project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "research.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graduation_research.settings")
 
 application = get_wsgi_application()
