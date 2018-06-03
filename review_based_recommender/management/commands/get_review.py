b2e8df181b3b9f3ea5f056e4bbc105eb894e69f6
xim0608 <xim.ryuki@gmail.com>
2018-06-03 10:42:15 +0900

    add window size setting

--- review_based_recommender/management/commands/get_review.py
+++ review_based_recommender/management/commands/get_review.py
@@ -24,6 +24,7 @@ class Command(BaseCommand):
         chrome_options = ChromeOptions()
         chrome_options.add_argument('--user-agent=' + self.user_agent)
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--window-size=1280,1024')
         self.spread_sheet = SpreadsheetData()
         self.browser = Chrome(options=chrome_options)
         self.wait = WebDriverWait(self.browser, self.delay)
