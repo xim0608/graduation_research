abbc22584432d48399715bb7589c14e36cd3df60
xim0608 <xim.ryuki@gmail.com>
2018-07-31 18:24:27 +0900

    add Spots page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <v-app>
     <Header/>
+    <v-content>
+      <Spots/>
+    </v-content>
   </v-app>
+
 </template>
 <script>
-import HelloWorld from './components/HelloWorld'
-import Header from './components/Header'
+  import Header from './components/Header'
+  import Spots from './components/Spots'
 
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Inspire'
+        }],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    },
+    name: 'App',
+    components: {
+      Header,
+      Spots
     }
-  },
-  name: 'App',
-  components: {
-    Header
   }
-}
 </script>
---
+++ src/components/Spots.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    Hello. This is Spots page.......
+  </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Spots"
+    }
+</script>
+
+<style scoped>
+
+</style>
