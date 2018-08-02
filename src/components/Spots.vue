ee11da5d886ca75bdc7f38ed3d2b1bc1932120cd
xim0608 <xim.ryuki@gmail.com>
2018-08-02 10:39:40 +0900

    search spots on enter

--- src/components/Spots.vue
+++ src/components/Spots.vue
@@ -16,7 +16,7 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
 
-          <b-nav-form>
+          <b-nav-form v-on:submit.prevent="search">
             <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" v-model="keyword"/>
             <b-button size="sm" class="my-2 my-sm-0" @click="search">Search</b-button>
           </b-nav-form>
