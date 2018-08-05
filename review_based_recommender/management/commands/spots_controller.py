b54cc597cb38474a2b1a3fb6d273f683fbc13b2a
xim0608 <xim.ryuki@gmail.com>
2018-08-05 11:16:05 +0900

    use city and remove some dead codes

--- review_based_recommender/management/commands/spots_controller.py
+++ review_based_recommender/management/commands/spots_controller.py
@@ -1,21 +1,16 @@
 from django.core.management.base import BaseCommand
-from ...models import Spot, Review, SpreadsheetData, CityTask
+from ...models import City
 import subprocess
-import random
-import os
 
 
 class Command(BaseCommand):
     def __init__(self):
         BaseCommand.__init__(self)
-        self.spreadsheet = SpreadsheetData()
         self.base_command = ['python', 'manage.py', 'get_spot', '--city-id']
 
     def handle(self, *args, **options):
         # 市内スポット一覧ページからスポットのurlを取得
-        urls = self.spreadsheet.get_set_city_url()
-        CityTask.import_urls(urls)
-        remained_cities = CityTask.objects.filter(finish=False)
+        remained_cities = City.objects.filter(finish=False)
         if len(remained_cities) > 0:
             city = remained_cities[0]
             print(city.base_id)
