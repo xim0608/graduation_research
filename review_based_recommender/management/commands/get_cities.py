9ecbf378a72b50f7796fdd9d600f5698c22cfa07
xim0608 <xim.ryuki@gmail.com>
2018-08-11 12:25:39 +0900

    get city headless

--- review_based_recommender/management/commands/get_cities.py
+++ review_based_recommender/management/commands/get_cities.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
         BaseCommand.__init__(self)
         chrome_options = ChromeOptions()
         chrome_options.add_argument('--user-agent=' + self.user_agent)
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--window-size=1280,1024')
         self.browser = Chrome(options=chrome_options)
         self.wait = WebDriverWait(self.browser, self.delay)
