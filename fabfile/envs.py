61d044687bab33da5c04942090f37aaf16272d2b
xim0608 <xim.ryuki@gmail.com>
2018-09-02 01:58:47 +0900

    automation pull script by fabric

---
+++ fabfile/envs.py
@@ -0,0 +1,11 @@
+from fabric.api import env
+from fabric.decorators import task
+
+@task
+def production():
+    home_dir = '/home/gr_user'
+    env.environment = "production"
+    env.app_path = home_dir + '/graduation_research'
+    env.daemon_name = 'graduation_research daemon'
+    env.user = 'gr_user'
+    env.hosts = ['worker01.idcf', 'worker02.idcf', 'worker03.idcf']
